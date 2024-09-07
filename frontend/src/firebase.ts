ca28ce7e81fbfa1ec2ce5e341fe3eebbdbd94c4d
잰진 <p_star16@naver.com>
2024-09-07 21:26:47 +0900

    fix: 안 쓰는 onMessage 주석 처리

--- frontend/src/firebase.ts
+++ frontend/src/firebase.ts
@@ -1,5 +1,6 @@
 import { initializeApp } from "firebase/app";
-import { getMessaging, Messaging, onMessage } from "firebase/messaging";
+import { getMessaging, Messaging } from "firebase/messaging";
+//import { onMessage } from "firebase/messaging";
 
 const firebaseConfig = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
