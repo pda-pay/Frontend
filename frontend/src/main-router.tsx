5b9db3731712970ee7b6ab916de4e736e6504761
YaRkyungmin <kkgm94@naver.com>
2024-09-09 11:57:06 +0900

    hot: 진짜 hot하다

--- frontend/src/main-router.tsx
+++ frontend/src/main-router.tsx
@@ -25,13 +25,13 @@ import { initializeApp } from "firebase/app";
 import { getMessaging, getToken, onMessage } from "firebase/messaging";
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
-  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
+  apiKey: "AIzaSyDbIsSgAmp3gseqS57ke89BgearBuJETiI",
+  authDomain: "onefourzero-9b623.firebaseapp.com",
+  projectId: "onefourzero-9b623",
+  storageBucket: "onefourzero-9b623.appspot.com",
+  messagingSenderId: "204793101998",
+  appId: "1:204793101998:web:f3d8c5393abfb6daccf73f",
+  measurementId: "G-8HL2VX6RSW",
 };
 
 const app = initializeApp(firebaseConfig);
@@ -54,13 +54,11 @@ if ("serviceWorker" in navigator) {
 
 const requestFCMToken = async () => {
   const permission = await Notification.requestPermission();
-  console.log("permission", permission);
-  console.log("vapid", import.meta.env.VITE_FIREBASE_VAPID_KEY);
 
   if (permission === "granted") {
     try {
       const token = await getToken(messaging, {
-        vapidKey: import.meta.env.VITE_FIREBASE_VAPID_KEY,
+        vapidKey: "BBvAL4OLtgr0yJFce3-d-Z7dBs12FQ1vLUj90HthWyS2JMFt6gp9pg9Qdm4eqF9hQ-oQh9ZNQSfxlj84N6cYHOc",
       });
       alert(token);
       console.log("FCM Token:", token);
