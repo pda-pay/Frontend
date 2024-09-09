c9af8cc8c2090c0bbbc4b905453c4dde1dbdad13
잰진 <p_star16@naver.com>
2024-09-09 10:17:24 +0900

    feat: 한도 변경 api 연결

--- frontend/src/routes/settinglimit/SettingLimitPage.tsx
+++ frontend/src/routes/settinglimit/SettingLimitPage.tsx
@@ -21,19 +21,24 @@ export default function SettingLimitPage() {
   const [mem, setMem] = useState<boolean>(false);
 
   //TODO: api GET요청으로 받아온 데이터
-  //현재한도, 최대한도, 담보
-  const data: [number, number, number] = [50000000, 50000000, 10000000000];
+  //현재한도, 최대한도, 담보금액
+  //const data: [number, number, number] = [50000000, 50000000, 10000000000];
   const [totalLimit, setTotalLimit] = useState<number>(0);
   const [currentLimit, setCurrentLimit] = useState<number>(0);
+  const [totalMortgagedPrice, setTotalMortgagedPrice] = useState<number>(0);
+  const [totalPaymentAmount, setTotalPaymentAmount] = useState<number>(0);
 
   //currentlimit 사용 안해서 빌드에러나서 콘솔로그찍음
   useEffect(() => {
     console.log(currentLimit);
   }, []);
 
-  const [limit, setLimit] = useState<number>(data[0]);
-  const [mortgageRate, setMortgageRate] = useState<number>(data[2] / limit);
+  //const [limit, setLimit] = useState<number>(data[0]);
+  const [mortgageRate, setMortgageRate] = useState<number>(
+    (totalMortgagedPrice / currentLimit) * 100
+  );
   const [errLimit, setErrLimit] = useState<boolean>();
+  const [errMsg, setErrMsg] = useState<string>("");
 
   const [inputValue, setInputValue] = useState<string>("");
 
@@ -61,6 +66,8 @@ export default function SettingLimitPage() {
         const data = response.data;
         setTotalLimit(data.totalLimit);
         setCurrentLimit(data.currentLimit);
+        setTotalMortgagedPrice(data.totalMortgagedPrice);
+        setTotalPaymentAmount(data.totalPaymentAmount);
       }
     } catch (error) {
       if (axios.isAxiosError(error)) {
@@ -75,9 +82,37 @@ export default function SettingLimitPage() {
     getLimit();
   }, []);
 
+  const putLimit = async (): Promise<boolean> => {
+    const temp = {
+      currentLimit: currentLimit,
+    };
+    try {
+      const response = await payjoinservice.putLimit(temp);
+
+      if (response.status === 200) {
+        return true;
+      } else return false;
+    } catch (error) {
+      console.log(error);
+      return false;
+    }
+  };
+
   const validateLimit = () => {
-    if (totalLimit > data[1] || totalLimit < 0 || mortgageRate < 140) {
+    if (
+      currentLimit > totalLimit ||
+      currentLimit < 0 ||
+      mortgageRate < 140 ||
+      totalPaymentAmount > currentLimit
+    ) {
       setErrLimit(true);
+      if (currentLimit > totalLimit)
+        setErrMsg("최대 한도 이하로 설정해주세요.");
+      else if (currentLimit < 0) setErrMsg("양수로 입력해주세요.");
+      else if (mortgageRate < 140)
+        setErrMsg("담보 유지 비율이 140% 이상이 되도록 설정해주세요.");
+      else if (totalPaymentAmount > currentLimit)
+        setErrMsg("결제 예정액 이상으로 설정해주세요.");
     } else {
       setErrLimit(false);
     }
@@ -89,19 +124,20 @@ export default function SettingLimitPage() {
 
     if (!isNaN(vToNum) && v !== " ") {
       setInputValue(v);
-      setLimit(vToNum);
+      setCurrentLimit(vToNum);
     } else {
+      setErrMsg("정수로 입력해주세요.");
       setErrLimit(true);
     }
   };
 
   useEffect(() => {
     validateLimit();
-  }, [limit, mortgageRate]);
+  }, [currentLimit, mortgageRate]);
 
   useEffect(() => {
-    setMortgageRate(data[2] / limit);
-  }, [limit]);
+    setMortgageRate((totalMortgagedPrice / currentLimit) * 100);
+  }, [currentLimit]);
 
   return (
     <PaddingDiv>
@@ -109,10 +145,19 @@ export default function SettingLimitPage() {
       <div>
         <NormalTitle>현재 고객님의 한도 현황입니다.</NormalTitle>
         <BackgroundFrame color="blue">
-          <BoldTitle>현재 한도: {limit} 원</BoldTitle>
-          <BoldTitle>최대 한도: {data[1]} 원</BoldTitle>
-          <NormalTitle>담보: {data[2]} 원</NormalTitle>
-          <NormalTitle>담보 유지 비율: {mortgageRate}%</NormalTitle>
+          <BoldTitle>현재 한도: {currentLimit.toLocaleString()} 원</BoldTitle>
+          <BoldTitle>최대 한도: {totalLimit.toLocaleString()} 원</BoldTitle>
+          <br />
+          <NormalTitle>
+            결제 예정액: {totalPaymentAmount.toLocaleString()} 원
+          </NormalTitle>
+          <br />
+          <NormalTitle>
+            담보: {totalMortgagedPrice.toLocaleString()} 원
+          </NormalTitle>
+          <NormalTitle>
+            담보 유지 비율: {(mortgageRate | 0).toLocaleString()}%
+          </NormalTitle>
         </BackgroundFrame>
         <div className="text-sm	text-gray-400">
           최대 한도를 늘리려면 담보를 더 잡아야 합니다.
@@ -123,22 +168,20 @@ export default function SettingLimitPage() {
         <BackgroundFrame color="blue">
           <div>
             <label className="block">
-              <span>최대 {data[1]}원 설정 가능합니다.</span>
+              <span className="text-sm text-gray-500">
+                최대 {totalLimit.toLocaleString()}원 설정 가능합니다.
+              </span>
               <input
                 type="text"
                 pattern="[0-9]*"
                 name="limit"
                 value={inputValue}
                 onChange={updateLimit}
-                placeholder={data[1].toString()}
+                placeholder={totalLimit.toString()}
                 className="mt-1 px-3 py-2 bg-white border shadow-sm border-slate-300 placeholder-slate-400 focus:outline-none focus:border-sky-500 focus:ring-sky-500 block w-full rounded-md sm:text-sm focus:ring-1"
               />
               {errLimit && (
-                <p className="mt-2 text-sm text-red-600">
-                  {
-                    "최대 한도 이하의 담보 유지 비율 140%이 되는 한도로 설정해주세요."
-                  }
-                </p>
+                <p className="mt-2 text-sm text-red-600">{errMsg}</p>
               )}
             </label>
           </div>
@@ -153,8 +196,9 @@ export default function SettingLimitPage() {
                 beforetext="취소"
                 nexttext="수정 완료"
                 beforeurl="/payment"
+                nextdisabled={errLimit}
                 nexturl="/payment"
-                //nextOnClick={putMorgagedStocks}
+                nextOnClick={putLimit}
               />
             ) : (
               <ButtonBar
@@ -163,7 +207,7 @@ export default function SettingLimitPage() {
                 beforeurl="/priority"
                 nextdisabled={errLimit}
                 nexturl="/payment"
-                //nextOnClick={putMorgagedStocks}
+                nextOnClick={putLimit}
               />
             )}
           </div>
@@ -174,7 +218,7 @@ export default function SettingLimitPage() {
             nexttext="완료"
             nextdisabled={errLimit}
             nexturl="/account"
-            //nextOnClick={putMorgagedStocks}
+            nextOnClick={putLimit}
           />
         )}
       </div>
