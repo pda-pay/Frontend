fe9bc9996e967e2aa0552ab8616516220a2d72ad
잰진 <p_star16@naver.com>
2024-09-10 23:52:49 +0900

    feat: 안정성 등급 안내

--- frontend/src/routes/stock/component/LevelInfoModal.tsx
+++ frontend/src/routes/stock/component/LevelInfoModal.tsx
@@ -21,13 +21,21 @@ export default function LevelInfoModal({
       <div className="mb-5">
         <BoldTitle>주식 안정성 안내</BoldTitle>
       </div>
-      <p>
+      {/* <p>
         <span className="font-bold">1등급:</span> 가장 안정성이 높은 종목, 한도
         65%
       </p>
       <p>
         <span className="font-bold">2등급:</span> 안정성이 높은 종목, 한도 60%
-      </p>
+      </p> */}
+      <div className="flex flex-col gap-2">
+        <p>각 안정성 그룹에 따라 한도는 다음과 같이 결정됩니다.</p>
+        <p> - 1그룹: 전일 종가의 65% </p>
+        <p> - 2그룹: 전일 종가의 60% </p>
+        <p> - 3그룹: 전일 종가의 55%</p>
+        <p>- 4그룹: 전일 종가의 50%</p>
+        <p>- 5그룹: 전일 종가의 40%</p>
+      </div>
     </BasicModal>
   );
 }
