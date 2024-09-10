44cb01ca45afbd3b9f49e3c338ed3a913946c83c
yapyap2 <wonwoo42@gmail.com>
2024-09-11 00:02:39 +0900

    fix: 자산 페이지 결제서비스 미가입시 접근차단

--- frontend/src/routes/asset/AssetPage.tsx
+++ frontend/src/routes/asset/AssetPage.tsx
@@ -5,16 +5,38 @@ import MortgageState from "./MortgageState";
 
 import axios from "axios";
 import StockPieChart from "./StockPieChart";
+import Swal from "sweetalert2";
+import { useNavigate } from "react-router-dom";
+
+interface checkMem {
+  name: string;
+  paymentAccess: boolean;
+  paymentServiceMember: boolean;
+  userId: string;
+}
 
 export default function AssetPage() {
   const service = new userAPI();
   const [name, setName] = useState<string>("익명");
+  const navigate = useNavigate();
 
   const getUserInfo = async () => {
     try {
       const response = await service.checkMem();
 
       if (response.status === 200) {
+        const data: checkMem = response.data;
+
+        if (!data.paymentServiceMember) {
+          Swal.fire({
+            icon: "warning",
+            title: `<span style="font-size: 20px; font-weight : bolder;">결제 서비스에<br/>가입하지 않으셨습니다.</span>`,
+            confirmButtonColor: "blue",
+          }).then(() => {
+            navigate(-1);
+          });
+        }
+
         setName(response.data.name);
       }
     } catch (error) {
