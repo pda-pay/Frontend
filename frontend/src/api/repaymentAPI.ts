329a65e3829c6805c67dce0de1486e1ded5f40b0
wlsdk9803 <wlsdk9803@naver.com>
2024-09-10 16:15:39 +0900

    feat: repayment history api url 수정

--- frontend/src/api/repaymentAPI.ts
+++ frontend/src/api/repaymentAPI.ts
@@ -3,7 +3,7 @@ import BaseApi from "./axiosInstance";
 export default class repaymentAPI extends BaseApi {
   async getRepaymentHistory(y: number, m: number) {
     const resp = await this.fetcher.get(
-      `/repayment/history?year=${y}&month=${m}`
+      `/payment/repayment-history?year=${y}&month=${m}`
     );
     return resp;
   }
